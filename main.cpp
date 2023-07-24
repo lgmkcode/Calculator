ee8553fac8fa95ab6022593e26504a866dd0df15
lgmk.code <halileee601@gmail.com>
2023-07-24 22:51:09 +0300

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "calculator.h"
+using namespace std;
+
+
+int main(){
+
+while(true){
+
+    calculator calculator1;
+    calculator1.get_input();
+    calculator1.calculate();
+}
+
+return 0;
+}
