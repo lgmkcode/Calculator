e3653464562bea545f7110eb2b724ef021d38736
lgmk.code <halileee601@gmail.com>
2023-07-26 12:12:58 +0300

    add switch-case

--- calculator.cpp
+++ calculator.cpp
@@ -3,27 +3,25 @@
 
 void calculator::calculate(){
 
-    if (symbol=='+'){
-        cout << " = " << sum(num1,num2) << endl;
-    }
-    else if (symbol=='*'){
-        cout << " = " << multi(num1, num2) << endl;
-    }
-    else if (symbol=='/'){
-        cout << " = " << division(num1, num2) << endl;
-    }
-    else if (symbol=='-'){
-        cout << " = " << subtraction(num1, num2) << endl;
-    }
-    else{
-        cout << "entered incorrect type" << endl;
+    switch(symbol){
+        case'+': cout << " = " << sum(num1, num2) << endl;
+            break;
+        case'*': cout << " = " << multi(num1, num2) << endl;
+            break;
+        case'/': cout << " = " << division(num1, num2) << endl;
+            break;
+        case'-': cout << " = " << subtraction(num1, num2) << endl;
+            break;
+        default: cout << " entered incorrect type!" << endl;
+            break;
     }
 }
+
 
 void calculator::get_input(){
     cout << "enter input.(example 3 * 5)" << endl;
     cin >> num1 >> symbol >> num2;
-    cout << num1 << symbol << num2;
+    cout << num1 << " " << symbol << " " << num2;
 }
 
 
